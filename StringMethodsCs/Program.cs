11e0f2ad5f88732e5eca5c4795df414e4f648edd
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-06 19:11:08 -0500

    substring and index of

--- StringMethodsCs/Program.cs
+++ StringMethodsCs/Program.cs
@@ -6,8 +6,19 @@ namespace StringMethodsCs
     {
         static void Main(string[] args)
         {
+            //Trim();
             string fullName = "Masen Funderburk ";
             Console.WriteLine("Trim: '{0}' ", fullName.Trim());
+            //ToUpper();
+            Console.WriteLine("ToUpper: '{0}' ", fullName.Trim().ToUpper());
+
+            int index = fullName.IndexOf(' ');
+            string firstName = fullName.Substring(0, index);
+            string lastName = fullName.Substring(index + 1);
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
+
+
 
         }
     }
