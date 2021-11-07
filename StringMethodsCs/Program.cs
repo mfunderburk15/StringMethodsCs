0aa9b2eaa65e8986bb2068f4c571799a13c42bfd
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-06 19:03:43 -0500

    First Commit

---
+++ StringMethodsCs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringMethodsCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
