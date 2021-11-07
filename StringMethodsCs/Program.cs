1a16b6e621dcc6a22fb86a02ebf5cff95af7a299
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-06 19:21:39 -0500

    split, isnullorwhitespace, string -> int, and int ->string

--- StringMethodsCs/Program.cs
+++ StringMethodsCs/Program.cs
@@ -18,6 +18,24 @@ namespace StringMethodsCs
             Console.WriteLine("First Name: " + firstName);
             Console.WriteLine("Last Name: " + lastName);
 
+            string[] names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("Masen", "Mase"));
+
+
+            if (String.IsNullOrWhiteSpace(null))
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            string str = "25";
+            int age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C0")); 
 
 
         }
